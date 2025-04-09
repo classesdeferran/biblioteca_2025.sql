cbbbd3d95faac6a614c9fbe0cb9b65f44bfdb18a
Ferran <ferran.docencia@gmail.com>
2025-04-09 09:05:35 +0200

    2025-04-09

--- biblioteca.sql
+++ biblioteca.sql
@@ -15,7 +15,7 @@ USE biblioteca;
 # 	-- fecha_incorporacion DEFAULT CURRENT_TIMESTAMP
 
 CREATE TABLE libros (
-id int NOT NULL AUTO_INCREMENT PRIMARY KEY.
+id int NOT NULL auto_increment PRIMARY KEY,
 titulo varchar(100) NOT NULL,
 autor_nombre varchar(50) NOT NULL,
 autor_apellido varchar(100) NULL,
@@ -23,7 +23,9 @@ editorial varchar(50) NOT NULL,
 year_edition YEAR,
 ejemplares smallint UNSIGNED,
 fecha_incorporacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
+
+DESCRIBE libros;
 
 # Inserta este libro:
 # Cien años de soledad
@@ -32,3 +34,12 @@ fecha_incorporacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 # 1990
 # Tusquets
 # 3
+
+INSERT INTO libros(titulo, autor_nombre, autor_apellido, editorial, year_edition, ejemplares) 
+VALUES ("Cien años de soledad", "Gabriel", "García Marquez", "Tusquets", 1990, 3);
+
+INSERT INTO libros(titulo, autor_nombre, autor_apellido, editorial, year_edition, ejemplares) 
+VALUES ("El coronel no tiene quien le escriba", "Gabriel", "García Marquez", "Tusquets", 1995, 2);
+
+
+SELECT * FROM libros;
